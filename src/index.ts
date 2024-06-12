9b2e2d71838fb343eea57bbd4adc04d5475fc944
niv_b <niv_b@datascope.co.il>
2024-06-12 12:40:56 +0300

    fixed

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { getDb } from './DataAccess/mongo.ts';
-import userRoutes from './Users/routes/userRoutes';
+import userRoutes from './Users/routes/userRoutes.ts';
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 const app = express();
